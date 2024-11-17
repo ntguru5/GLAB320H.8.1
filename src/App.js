a9a3298ef625bbdeb44042c17d252cfb490d4e71
Ken Yeh <yehken@gmail.com>
2024-11-16 21:32:58 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App () {
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
-        <Route path="/price" element={<Price/>}/>
+        <Route path="/price/:symbol" element={<Price/>}/>
       </Routes>
     </div>
   );
